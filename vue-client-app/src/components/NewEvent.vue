dfc3434586961010a221a3153dcc46c7b3fee4ff
Freeze <rain-ilia@rambler.ru>
2023-02-08 02:44:32 +0100

    NewEvent.vue -> dialog for new events

---
+++ vue-client-app/src/components/NewEvent.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="root-div">
+    <div class="overlay" @click="closeDialog">
+    </div>
+    <div class="modal">
+      <h2 class="title">New event</h2>
+      <div class="fields">
+        <label class="field-label">
+          <span>Title</span>
+          <input type="text" v-model="localEvent.title">
+        </label>
+        <label class="field-label">
+          <span>Description</span>
+          <input type="text" v-model="localEvent.description">
+        </label>
+        <label class="field-label">
+          <span>Location</span>
+          <input type="text" v-model="localEvent.location">
+        </label>
+
+
+        <label class="field-label">
+          <span>Date</span>
+          <input type="datetime-local" v-model="localEvent.date">
+        </label>
+
+
+        <label class="field-label">
+          <span>Status</span>
+
+
+          <select v-model="localEvent.status">
+            <option v-for="option in options" :key="option">
+              {{ option }}
+            </option>
+          </select>
+
+
+        </label>
+
+      </div>
+
+      <div class="buttons">
+        <button type="button" class="btn decline" @click="closeDialog">Cancel</button>
+        <button type="button" class="btn accept" @click="saveDialog">Create</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "NewEvent",
+  data() {
+    return {
+      localEvent: {
+        status:'DRAFT'
+      },
+      options: ["DRAFT", "PRIVATE", "PUBLIC", "REMOVED", "UNDEFINED"]
+    }
+  },
+  methods: {
+    closeDialog() {
+      this.$emit('closeDialog')
+    },
+    async saveDialog() {
+      this.localEvent.owner = localStorage.getItem('username')
+      await this.$http.post('http://127.0.0.1:8000/events/', {...this.localEvent}, {
+        headers: {Authorization: `Token ${localStorage.getItem('token')}`}
+      })
+          .then(r=> this.$emit('requestFetchNewEvent', r.data.url))
+          .then(()=> this.closeDialog())
+          .catch(e=> console.log(e))
+    },
+  },
+  created() {
+    const dateString = new Date().toISOString()
+    this.localEvent.date = dateString.substring(0, dateString.length-1)
+  },
+}
+</script>
+
+<style scoped>
+.root-div {
+
+}
+
+.overlay {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: rgba(15, 15, 15, 0.85);
+  z-index: 9;
+}
+
+.modal {
+  position: fixed;
+  width: 50%;
+  height: auto;
+  top: 50%;
+  left: 50%;
+  background: darkolivegreen;
+  transform: translate(-50%, -50%);
+  z-index: 10;
+  border-radius: 5px;
+  padding: 10px;
+  box-shadow: 2px 2px black;
+}
+
+.title {
+  text-align: center;
+  color: white;
+}
+
+.fields {
+  margin: 5px
+}
+
+.buttons {
+  display: flex;
+  align-items: center;
+
+}
+
+.field-label {
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+.field-label span {
+  width: 100px;
+  flex: 0 0 100px;
+  text-shadow: 2px 2px 2px black;
+}
+
+.field-label input, .field-label select {
+  width: 100%;
+}
+
+.btn {
+  margin: 5px;
+  border-radius: 5px;
+  min-height: 36px;
+  min-width: 72px;
+}
+
+.accept {
+  background: green;
+  color: #ffffff;
+  margin-left: auto;
+}
+
+.decline {
+  background: darkred;
+  color: #c8c8c8
+}
+</style>
